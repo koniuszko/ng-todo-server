a1213cfac4189e0aad9d978b68c182c133ca581a
Mateusz Koniuszaniec <koniuszaniec.mateusz@gmail.com>
2023-05-29 20:47:24 +0200

    init

---
+++ ng-todo-server/Controllers/TaskControler.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ng_todo_server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TaskController : ControllerBase
+{
+   
+    private readonly ILogger<TaskController> _logger;
+
+    public TaskController(ILogger<TaskController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetTasks")]
+    public IEnumerable<Task> Get()
+    {
+        
+    }
+}
---
+++ ng-todo-server/Task.cs
@@ -0,0 +1,12 @@
+namespace ng_todo_server;
+
+public class Task
+{
+    
+    public int id { get; set; }
+    
+    public string taskName { get; set; }
+    
+    public bool isDone { get; set; }
+    
+}
